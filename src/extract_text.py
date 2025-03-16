fc9ebc99eab9bec705a59c705eb7ed8af2020414
gupta-vivek <23401083+gupta-vivek@users.noreply.github.com>
2025-03-16 21:21:18 +0100

    feat: Modify extract text method to extract sections based on document content

--- src/extract_text.py
+++ src/extract_text.py
@@ -1,39 +1,45 @@
+"""Extract text from PDF files."""
+
+import json
+import re
+
 import fitz
 
 
-def extract_text_from_pdf(pdf_path: str) -> str:
+def extract_sections(sections, document_path):
     """
-    Extract text from pdf file
-    :param pdf_path: Path to pdf file
-    :return: extracted text
-    """
-    doc = fitz.open(pdf_path)
-    text = ""
-    for page in doc:
-        text += page.get_text()
-    return text
+    Extracts sections of text from the PDF document based on section titles and their
+    corresponding page numbers.
 
+    :param sections: A list of sections with their title and page number.
+    :param document_path: Path to the PDF document.
+    :return: A list of dictionaries, where each dictionary contains the section title and the
+        extracted and cleaned text.
+    :rtype: list of dict
+    """
+    doc = fitz.open(document_path)
+    len_sections = range(len(sections) - 1)
+    documents = []
+    for i in len_sections:
+        start_title, start_page_num = sections[i].split(',')
+        end_title, end_page_num = sections[i + 1].split(',')
+        text = ""
+        if start_page_num == end_page_num:
+            end_page_num = int(end_page_num) + 1
+        for page in doc[int(start_page_num):int(end_page_num)]:
+            page_text = page.get_text()
+            text += page_text
+        text = re.sub(r'-\n', '', text)  # Fix hyphenated words split across lines
+        documents.append({"section": start_title, "text": text})
 
-def preprocess_text(input_text: list[str], out_path: str):
-    """
-    Preprocess the text and save it in a text file
-    :param input_text: input text
-    :param out_path: output path
-    :return:
-    """
-    with open(out_path, "w", encoding="utf-8") as f:
-        for line in input_text:
-            line = line.strip()
-            line = " ".join(line.split())
-            if line or len(line) > 1:
-                f.write(line + "\n")
+    return documents
 
 
 if __name__ == "__main__":
-    # document = extract_text_from_pdf("../data/instructions.pdf")
-    # with open('../document.txt', 'w', encoding="utf-8") as f:
-    #     f.write(document)
+    document_path = "../data/Kompendium1.pdf"
+    with open('../data/sections.txt', 'r', encoding='utf-8') as f:
+        sections = f.read().lower().splitlines()
 
-    with open("../data/document.txt", "r", encoding="utf-8") as f:
-        text = f.readlines()
-        preprocess_text(text, "../data/cleaned_doc.txt")
+    documents = extract_sections(sections, document_path)
+    with open('../data/document.json', 'w', encoding='utf-8') as f:
+        json.dump(documents, f)
